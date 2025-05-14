a65020e11bc9a290002d23e8a2b67e8cbabe7fef
vic <vic.salak@gmail.com>
2025-05-14 22:50:30 +0800

    updated and fixed the api

--- api/src/index.js
+++ api/src/index.js
@@ -3,13 +3,3 @@ const { app } = require('@azure/functions');
 app.setup({
   enableHttpStream: true,
 });
-
-// Register the streamIfc function
-const streamIfc = require('./streamIfc');
-app.http('streamIfc', {
-  methods: ['GET'],
-  authLevel: 'anonymous',
-  handler: async (request, context) => {
-    return await streamIfc(request, context);
-  }
-});
